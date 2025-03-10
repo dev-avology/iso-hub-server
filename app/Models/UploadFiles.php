2276c81a4ad6a2d4e88be84b5014e0ce1d379af2
dev-avology <devavology12@gmail.com>
2025-03-10 15:05:07 +0530

    added

---
+++ app/Models/UploadFiles.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UploadFiles extends Model
+{
+    use HasFactory;
+    protected $table = 'uploaded_files';
+    protected $fillable = [
+        'user_id',
+        'file_path',
+        'uploaded_at'
+    ];
+}
