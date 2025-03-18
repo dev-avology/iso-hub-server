3bb74dcf0c479f3809a62630212654e2907846aa
dev-avology <devavology12@gmail.com>
2025-03-18 15:50:25 +0530

    added

--- cron_job.php
+++ cron_job.php
@@ -1,7 +1,8 @@
 <?php
 
 // Load Composer's autoload file
-require __DIR__ . '/../vendor/autoload.php';
+// require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Load the Laravel application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
