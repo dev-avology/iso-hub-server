af164a2d50e59b7f231a1a3fa3c202fc6540ef5c
dev-avology <devavology12@gmail.com>
2025-03-18 10:46:52 +0530

    added

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Api\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/upload-files', [FileController::class, 'uploadFiles']);
+Route::post('file/upload-files', [FileController::class, 'uploadFiles']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
