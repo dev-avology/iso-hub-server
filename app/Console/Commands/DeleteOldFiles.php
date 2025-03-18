2b1f659f6a1b0995e4299c27a61962285332df03
dev-avology <devavology12@gmail.com>
2025-03-18 16:07:23 +0530

    added

--- app/Console/Commands/DeleteOldFiles.php
+++ app/Console/Commands/DeleteOldFiles.php
@@ -7,6 +7,7 @@ use App\Models\UploadFiles;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 
 class DeleteOldFiles extends Command
 {
@@ -16,19 +17,18 @@ class DeleteOldFiles extends Command
     public function handle()
     {
         // Get all files older than 180 days
-        // $oldFiles = UploadFiles::where('created_at', '<', Carbon::now()->subDays(1))->get();
+        $oldFiles = UploadFiles::where('created_at', '<', Carbon::now()->subDays(1))->get();
 
-        // foreach ($oldFiles as $file) {
-        //     $filePath = public_path($file->file_path);
-        //     // Check if the file exists and delete it
-        //     if (File::exists($filePath)) {
-        //         File::delete($filePath);
-        //     }
-        //     // Delete record from database
-        //     $file->delete();
-        // }
-        \Log::info('Command running');
-
-        // $this->info('Old files cleanup completed.');
+        foreach ($oldFiles as $file) {
+            $filePath = public_path($file->file_path);
+            // Check if the file exists and delete it
+            if (File::exists($filePath)) {
+                File::delete($filePath);
+            }
+            // Delete record from database
+            $file->delete();
+            Log::info('yes deleted file by cron job');
+        }
+        Log::info('yes not delted file by cron job');
     }
 }
