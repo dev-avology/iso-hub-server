6c5af270f329b412871ed6d2ca4e7e63c519fc2e
dev-avology <devavology12@gmail.com>
2025-04-11 15:17:39 +0530

    added

--- app/Services/JotFormService.php
+++ app/Services/JotFormService.php
@@ -16,21 +16,63 @@ use Illuminate\Support\Str;
 
 class JotFormService
 {
-    public function create($request,$user_id)
+    public function create($request, $user_id)
     {
         $form = JotForm::create([
-            'dba' => $request->dba,
-            'description' => $request->description,
-            'address2' => $request->address2,
-            'state' => $request->state,
-            'city' => $request->city,
-            'pincode' => $request->pincode,
-            'is_same_shipping_address' => $request->is_same_shipping_address,
             'signature_date' => $request->signature_date,
             'signature' => $request->signature,
             'user_id' => $user_id,
-            'is_duplicate' => isset($request->is_duplicate) ? '1' : '0'
+            'is_duplicate' => isset($request->is_duplicate) ? '1' : '0',
+            'business_type_other' => $request->business_type_other ?? '',
+
+            'business_dba' => $request->business_dba ?? '',
+            'business_corporate_legal_name' => $request->business_corporate_legal_name ?? '',
+            'business_location_address' => $request->business_location_address ?? '',
+            'business_corporate_address' => $request->business_corporate_address ?? '',
+            'business_city' => $request->business_city ?? '',
+            'business_state' => $request->business_state ?? '',
+            'business_zip' => $request->business_zip ?? '',
+            'business_phone_number' => $request->business_phone_number ?? '',
+            'business_contact_name' => $request->business_contact_name ?? '',
+            'business_contact_number' => $request->business_contact_number ?? '',
+            'business_start_date' => $request->business_start_date ?? '',
+            'business_tax_id' => $request->business_tax_id ?? '',
+            'business_profile_business_type' => json_encode($request->business_profile_business_type) ?? '',
+
+            'ownership_owner_name' => $request->ownership_owner_name ?? '',
+            'ownership_title' => $request->ownership_title ?? '',
+            'ownership_percent' => $request->ownership_percent ?? '',
+            'ownership_phone_number' => $request->ownership_phone_number ?? '',
+            'ownership_city' => $request->ownership_city ?? '',
+            'ownership_state' => $request->ownership_state ?? '',
+            'ownership_zip' => $request->ownership_zip ?? '',
+            'ownership_email' => $request->ownership_email ?? '',
+            'ownership_dob' => $request->ownership_dob ?? '',
+            'ownership_social_security_number' => $request->ownership_social_security_number ?? '',
+            'ownership_residential_street_address' => $request->ownership_residential_street_address ?? '',
+            'ownership_driver_licence_number' => $request->ownership_driver_licence_number ?? '',
+
+            'bank_name' => $request->bank_name ?? '',
+            'aba_routing' => $request->aba_routing ?? '',
+            'doa' => $request->doa ?? '',
+
+            'business_type' => json_encode($request->business_type) ?? '',
+            'processing_services' => json_encode($request->processing_services) ?? '',
+
+            'terminal' => json_encode($request->terminal) ?? '',
+            'terminal_special_features' => $request->terminal_special_features ?? '',
+            'terminal_type_or_model' => $request->terminal_type_or_model ?? '',
+            'mobile_app' => json_encode($request->mobile_app) ?? '',
+            'mobile_app_special_features' => $request->mobile_app_special_features ?? '',
+            'mobile_app_cardreader_type_model' => $request->mobile_app_cardreader_type_model ?? '',
+            'pos_point_of_sale' => json_encode($request->pos_point_of_sale) ?? '',
+            'pos_special_features' => $request->pos_special_features ?? '',
+            'system_type_model' => $request->system_type_model ?? '',
+            'number_of_stations' => $request->number_of_stations ?? '',
+            'pos_other_items' => $request->pos_other_items ?? '',
+            'virtual_terminal' => json_encode($request->virtual_terminal) ?? '',
         ]);
+
         return $form;
     }
 }
---
+++ database/migrations/2025_04_11_090657_add_business_ownership_bank_and_terminal_fields_to_jot_forms_table.php
@@ -0,0 +1,122 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('jot_forms', function (Blueprint $table) {
+            $table->string('business_dba')->nullable()->after('status');
+            $table->string('business_corporate_legal_name')->nullable()->after('business_dba');
+            $table->text('business_location_address')->nullable()->after('business_corporate_legal_name');
+            $table->text('business_corporate_address')->nullable()->after('business_location_address');
+            $table->string('business_city')->nullable()->after('business_corporate_address');
+            $table->string('business_state')->nullable()->after('business_city');
+            $table->string('business_zip')->nullable()->after('business_state');
+            $table->string('business_phone_number')->nullable()->after('business_zip');
+            $table->string('business_contact_name')->nullable()->after('business_phone_number');
+            $table->string('business_contact_number')->nullable()->after('business_contact_name');
+            $table->string('business_start_date')->nullable()->after('business_contact_number');
+            $table->string('business_tax_id')->nullable()->after('business_start_date');
+            $table->text('business_profile_business_type')->nullable()->after('business_tax_id');
+
+            $table->string('ownership_owner_name')->nullable()->after('business_profile_business_type');
+            $table->string('ownership_title')->nullable()->after('ownership_owner_name');
+            $table->string('ownership_percent')->nullable()->after('ownership_title');
+            $table->string('ownership_phone_number')->nullable()->after('ownership_percent');
+            $table->string('ownership_city')->nullable()->after('ownership_phone_number');
+            $table->string('ownership_state')->nullable()->after('ownership_city');
+            $table->string('ownership_zip')->nullable()->after('ownership_state');
+            $table->string('ownership_email')->nullable()->after('ownership_zip');
+            $table->date('ownership_dob')->nullable()->after('ownership_email');
+            $table->string('ownership_social_security_number')->nullable()->after('ownership_dob');
+            $table->text('ownership_residential_street_address')->nullable()->after('ownership_social_security_number');
+            $table->string('ownership_driver_licence_number')->nullable()->after('ownership_residential_street_address');
+
+            $table->string('bank_name')->nullable()->after('ownership_driver_licence_number');
+            $table->string('aba_routing')->nullable()->after('bank_name');
+            $table->string('doa')->nullable()->after('aba_routing');
+
+            $table->text('business_type')->nullable()->after('doa');            
+
+            $table->text('processing_services')->nullable()->after('business_type');
+
+            $table->text('terminal')->nullable()->after('processing_services');
+            $table->string('terminal_special_features')->nullable()->after('terminal');
+            $table->string('terminal_type_or_model')->nullable()->after('terminal_special_features');
+
+            $table->text('mobile_app')->nullable()->after('terminal_type_or_model');
+            $table->string('mobile_app_special_features')->nullable()->after('mobile_app');
+            $table->string('mobile_app_cardreader_type_model')->nullable()->after('mobile_app_special_features');
+
+            $table->text('pos_point_of_sale')->nullable()->after('mobile_app_cardreader_type_model');
+            $table->string('pos_special_features')->nullable()->after('pos_point_of_sale');
+            $table->string('system_type_model')->nullable()->after('pos_special_features');
+            $table->string('number_of_stations')->nullable()->after('system_type_model');
+            $table->string('pos_other_items')->nullable()->after('number_of_stations');
+
+            $table->text('virtual_terminal')->nullable()->after('pos_other_items');
+            $table->string('business_type_other')->nullable()->after('virtual_terminal');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jot_forms', function (Blueprint $table) {
+            $table->dropColumn([
+                'business_dba',
+                'business_corporate_legal_name',
+                'business_location_address',
+                'business_corporate_address',
+                'business_city',
+                'business_state',
+                'business_zip',
+                'business_phone_number',
+                'business_contact_name',
+                'business_contact_number',
+                'business_start_date',
+                'business_tax_id',
+                'business_profile_business_type',
+                'ownership_owner_name',
+                'ownership_title',
+                'ownership_percent',
+                'ownership_phone_number',
+                'ownership_city',
+                'ownership_state',
+                'ownership_zip',
+                'ownership_email',
+                'ownership_dob',
+                'ownership_social_security_number',
+                'ownership_residential_street_address',
+                'ownership_driver_licence_number',
+                'bank_name',
+                'aba_routing',
+                'doa',
+                'business_type',
+                'processing_services',
+                'terminal',
+                'terminal_special_features',
+                'terminal_type_or_model',
+                'mobile_app',
+                'mobile_app_special_features',
+                'mobile_app_cardreader_type_model',
+                'pos_point_of_sale',
+                'pos_special_features',
+                'system_type_model',
+                'number_of_stations',
+                'pos_other_items',
+                'virtual_terminal',
+            ]);
+        });
+    }
+};
