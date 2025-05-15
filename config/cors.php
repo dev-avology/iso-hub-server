13fa017ff16e8fd7fea13e2bf83fefe9cf5c5bce
dev-avology <devavology12@gmail.com>
2025-05-15 13:21:33 +0530

    added

--- config/cors.php
+++ config/cors.php
@@ -2,35 +2,22 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'https://isohub.io',
-        'http://127.0.0.1:8000',  // ✅ Allow local frontend
-        'http://localhost:8000',  // ✅ Optional - sometimes browsers use localhost
+        'http://127.0.0.1:8000',
+        'http://localhost:8000',
+        'https://phpstack-1180784-5314741.cloudwaysapps.com'
     ],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
